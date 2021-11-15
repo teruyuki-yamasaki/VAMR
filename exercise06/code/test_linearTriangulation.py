daf3803f00c254c1b9f6650fb0c8e2712e19b44d
yukiCH <46243965+teruyuki-yamasaki@users.noreply.github.com>
2021-11-15 18:19:28 +0100

    Update test_linearTriangulation.py

--- exercise06/code/test_linearTriangulation.py
+++ exercise06/code/test_linearTriangulation.py
@@ -1,128 +1,32 @@
 import numpy as np 
+from linearTriangulation import linearTriangulation 
 
-N = 10 #;         % Number of 3-D points
-
-# Test linear triangulation
-#P = randn(4,N);  % Homogeneous coordinates of 3-D points
-#P(3, :) = P(3, :) * 5 + 10;
-#P(4, :) = 1;
-
-P = np.random.randn(4, N)
-P[2, :] = P[2, :] * 5 + 10 
-P[3, :] = 1     
-
-M1 =  np.array([
-        [500, 0, 320, 0],
-        [0, 500, 240, 0],
-        [0, 0, 1, 0]])
-
-M2 =  np.array([
-        [500, 0, 320, -100,],
-        [0, 500, 240, 0],
-        [0, 0, 1, 0]]) 
-				
-p1 = M1 @ P    #% Image (i.e., projected) points
-p2 = M2 @ P
-
-p1 = (p1 / p1[2,:])
-p2 = (p2 / p2[2,:])
-
-def skew0(p):
-    op = np.array([
-        [[0,0,0],[0,0,1],[0,-1,0]],
-        [[0,0,-1],[0,0,0],[1,0,0]],
-        [[0,1,0],[-1,0,0],[0,0,0]]
-    ])
-
-    px = op @ p
-
-    if 0:
-        print(op.shape)
-        print(p.shape)
-        print(px) 
-    return px
-
-p1xM1_0 = skew0(p1[:,0]) @ M1
-p2xM2_0 = skew0(p2[:,0]) @ M2
-A0 = np.vstack((p1xM1_0, p2xM2_0))
-#print(A0)
-tAA0 = np.transpose(A0, axes=(1,0)) @ A0
-#print(tAA0)
-U0, S0, V0 = np.linalg.svd(tAA0)
-print(V0)
-P0 = V0[-1,:] / V0[-1,-1] 
-print(P0)
-print(P[:,0])
-
-def skew(p):
-    op = np.array([
-        [[0,0,0],[0,0,1],[0,-1,0]],
-        [[0,0,-1],[0,0,0],[1,0,0]],
-        [[0,1,0],[-1,0,0],[0,0,0]]
-    ])
-
-    px = np.transpose(op @ p.T, axes=(0,2,1)).reshape(-1,3,3)
-
-    if 0:
-        print(op.shape)
-        print(p.shape)
-        print(px) 
+def run_test_linearTriangulatin(func):
+    # Test linear triangulation
     
-    return px
+    N = 10 # Number of 3-D points
 
-#print(P)
+    P = np.random.randn(4, N)
+    P[2, :] = P[2, :] * 5 + 10 
+    P[3, :] = 1     
 
-p1xM1_0 = skew(p1[:,0].reshape(1,3)) @ M1
-p2xM2_0 = skew(p2[:,0].reshape(1,3)) @ M2
-#A0 = np.vstack((p1xM1_0, p2xM2_0), axis=1)
-A0 = np.concatenate([p1xM1_0, p2xM2_0], axis=-2)
-#print(A0)
-tAA0 = np.transpose(A0, axes=(0,2,1)) @ A0
-#print(tAA0)
-U0, S0, V0 = np.linalg.svd(tAA0)
-#print(V0)
-P0 = V0[:,-1,:] / V0[:,-1,-1] 
-print(P0)
-print(P[:,0])
+    M1 =  np.array([
+            [500, 0, 320, 0],
+            [0, 500, 240, 0],
+            [0, 0, 1, 0]])
 
-def skew(p):
-    op = np.array([
-        [[0,0,0],[0,0,1],[0,-1,0]],
-        [[0,0,-1],[0,0,0],[1,0,0]],
-        [[0,1,0],[-1,0,0],[0,0,0]]
-    ])
+    M2 =  np.array([
+            [500, 0, 320, -100,],
+            [0, 500, 240, 0],
+            [0, 0, 1, 0]]) 
+                    
+    p1 = M1 @ P    #% Image (i.e., projected) points
+    p2 = M2 @ P
 
-    px = np.transpose(op @ p, axes=(0,2,1)) #.reshape(-1,3,3)
-    px = np.transpose(np.concatenate(px.T, axis=-1).reshape(-1,3,3), axes=(0,2,1)) 
+    P_est = func(p1,p2,M1,M2)
 
-    if 0:
-        print(op.shape)
-        print(p.shape)
-        print(px) 
-    
-    return px
+    print('P_est-P=\n')
+    print(P_est-P.T)
 
-#print(P)
-
-i = 0
-j = 4
-p1xM1 = skew(p1[:,i:j]) @ M1
-p2xM2 = skew(p2[:,i:j]) @ M2
-#A0 = np.vstack((p1xM1_0, p2xM2_0), axis=1)
-A = np.concatenate([p1xM1, p2xM2], axis=-2)
-#print(A0)
-tAA = np.transpose(A, axes=(0,2,1)) @ A
-#print(tAA0)
-U, S, V = np.linalg.svd(tAA)
-#print(V)
-
-i = 3
-Pi = V[i,-1,:] / V[i,-1,-1]
-print(Pi) 
-print(P[:,i])
-
-print(V[:,-1,:].shape)
-print(V[:,-1,-1].shape)
-print(V[:,-1,:] / (V[:,-1,-1]).reshape(-1,1)) 
-P_sol = V[:,-1,:] / V[:,-1,-1].reshape(-1,1)
-print(P_sol)
+if __name__=="__main__":
+    run_test_linearTriangulatin(linearTriangulation)

a5d142401fa9658472d379b6af9f47bbaa349063
yukiCH <46243965+teruyuki-yamasaki@users.noreply.github.com>
2021-11-16 15:19:44 +0100

    Update linearTriangulation.py

--- exercise06/code/linearTriangulation.py
+++ exercise06/code/linearTriangulation.py
@@ -23,13 +23,13 @@ def skew(p):
 def linearTriangulation(p1, p2, M1, M2):
     # LINEARTRIANGULATION  Linear Triangulation
     # Input:
-    #  - p1(N, 3): homogeneous coordinates of points in image 1
-    #  - p2(N, 3): homogeneous coordinates of points in image 2
+    #  - p1(3, N): homogeneous coordinates of points in image 1
+    #  - p2(3, N): homogeneous coordinates of points in image 2
     #  - M1(3,4): projection matrix corresponding to first image
     #  - M2(3,4): projection matrix corresponding to second image
     #
     # Output:
-    #  - P(N, 4): homogeneous coordinates of 3-D points
+    #  - P(4, N): homogeneous coordinates of 3-D points
 
     #TODO: create skew symmetric matrices of p1 and p2 
     p1x = skew(p1) 

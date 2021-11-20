d97389d997b3bce192b76b7c804e4b93b5fcddc6
yukiCH <46243965+teruyuki-yamasaki@users.noreply.github.com>
2021-11-20 23:59:14 +0100

    Update main.py

--- exercise01/code/main.py
+++ exercise01/code/main.py
@@ -2,136 +2,104 @@ import cv2
 import numpy as np 
 import matplotlib.pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
-from scipy.spatial.transform import Rotation
 import glob
 
-def txt2array(txt, sep=' '):
-    return np.fromstring(txt, dtype=float, sep=sep)
+def main():
+    # parameters 
+    args = {
+        'K': '../data/K.txt',
+        'D': '../data/D.txt',
+        'poses': '../data/poses.txt',
+        'images': '../data/images/*.jpg',
+        'undistorted_image': '../data/images_undistorted/img_0001.jpg',
+        'grid_shape': (6,9),
+        'unit_size': 0.04,
+    }
 
-def imshow(img, name="img"):
-    cv2.imshow(name, img)
+    # Load data 
+    K = txt2array(args['K']).reshape(3,3)           # camera parameters 
+    D = txt2array(args['D'])                        # distortion parameters 
+    poses = txt2array(args['poses']).reshape(-1, 6) # camera poses 
+    filenames = sorted(glob.glob(args['images']))   # image file names 
 
-    while True:
-        if cv2.waitKey(1)==ord('q'):
-            break 
-    cv2.destroyAllWindows()
-
-def imshow2(imgs):
-    interp = ["without bilinear interpolation", "with bilinear interpolation"]
-    fig, axs = plt.subplots(1, len(imgs), figsize=(20, 5)) 
-    for i, ax in enumerate(axs):
-        ax.imshow(imgs[i])
-        ax.set_title(interp[i])
-    plt.show()
-
-def imshow2_vertical(imgs):
-    fig, axs = plt.subplots(nrows=2, sharex=True, figsize=(10, 10)) 
-    axs[0].set_title('without bilinear interpolation')
-    axs[0].imshow(imgs[0])
-
-    axs[1].set_title('with bilinear interpolation')
-    axs[1].imshow(imgs[1])
-    plt.show()
-
-def add_corners(img, p, r=5, color=(0,0,255), thickness=-1):
-    for x,y in zip(p[0], p[1]):
-        img = cv2.circle(img, (round(x), round(y)), r, color, thickness)
-    return img 
-
-def add_cube(img, p, r=5, color=(0,0,255)):
-    A = [0, 0, 1, 2, 4, 4, 5, 6, 0, 1, 2, 3]
-    B = [1, 2, 3, 3, 5, 6, 7, 7, 4, 5, 6, 7]  
-
-    for i, j in zip(A, B):
-        x0 = round(p[0, i]); y0 = round(p[1, i])
-        x1 = round(p[0, j]); y1 = round(p[1, j])
-        img = cv2.line(img, (x0, y0), (x1, y1), color, r)
+    # Show loaded data 
+    if 1:
+        print('\n K=\n', K)
+        print('\n D=\n', D)
+        print('\n poses=\n', poses)
+        print('\n image names=\n') 
+        for i in range(len(filenames)): print(filenames[i]) 
     
-    return img 
-
-def plotXYZ(XX, YY, ZZ): # ZZ = f(XX, YY) 
-    fig = plt.figure()
-    ax = Axes3D(fig)
-    ax.plot_wireframe(XX, YY, ZZ)
-    plt.show()
-
-def swapXY(P):
-    P_ = np.zeros_like(P)
-    P_[:] = P[:]
-    P_[:,0] = P[:,1]
-    P_[:,1] = P[:,0] 
-    return P_
-
-def create_corner_dots(H=6, W=9, dx=0.04, dy=0.04):
-    X = np.arange(H) * dx
-    Y = np.arange(W) * dy 
-
-    XX, YY = np.meshgrid(X, Y) 
-    ZZ = XX*0 + YY*0  
-
-    XYZ = np.stack((XX, YY, ZZ), axis=-1).reshape(-1, 3)
-
-    YXZ = swapXY(XYZ) 
-
-    return YXZ 
-
-def create_cube_dots(xy=(0,0), size=1, dx=0.04, dy=0.04):
-
-    X = np.array([xy[0], xy[0]+size]) * dx 
-    Y = np.array([xy[1], xy[1]+size]) * dy
-
-    XX, YY = np.meshgrid(X, Y) 
-    ZZ0 = XX*0 + YY*0  
-    ZZ1 = -0.5 * size * (np.ones_like(XX) * dx + np.ones_like(YY) * dy)
-
-    XYZ0 = np.stack((XX, YY, ZZ0), axis=-1).reshape(-1, 3)
-    XYZ1 = np.stack((XX, YY, ZZ1), axis=-1).reshape(-1, 3)
-
-    XYZ = np.stack((XYZ0, XYZ1)).reshape(-1,3)
-
-    YXZ = swapXY(XYZ)
-
-    return YXZ
-
-def vec2mat(w):
-    theta = np.linalg.norm(w)
-
-    k = w / theta
-
-    kx = np.array([
-        [0, -k[2], k[1]],
-        [k[2], 0, -k[0]],
-        [-k[1], k[0], 0]
-    ])
-
-    R = np.eye(3) + np.sin(theta) * kx + (1 - np.cos(theta)) * (kx@kx)
+    if 0:
+        # undistorted image    
+        img_undistorted = imread(args['undistorted_image']) 
+        imshow(img_undistorted)
+        imsave(img_undistorted, '../results/undistorted.png')
     
-    return R 
+        # add grid corners 
+        Rt = poseVector2TransformationMatrix(poses[0])
+        cornersXYZ = createGridCorners(args['grid_shape'], args['unit_size']) 
+        cornersxy = projectPoints(K, Rt, cornersXYZ)
 
-def poseVector2TransformationMatrix(pose):
-    w = pose[:3]
-    t = pose[3:] 
-    R = vec2mat(w) #R = np.array(Rotation.from_rotvec(w).as_matrix())
-    Rt = np.concatenate([R, t.reshape(1,3).T], axis=1)
-    Rt = np.concatenate([Rt, np.array([[0, 0, 0, 1]])], axis=0) 
-    return Rt
+        img_undistorted = imdots(img_undistorted, cornersxy) 
+        imshow(img_undistorted)
+    
+        # add a cube 
+        cubeXYZ, corr = createCubeCorners(Oyx=(2,3), size=3, unit_size=args['unit_size']) 
+        cubexy = projectPoints(K, Rt, cubeXYZ) 
+        img_undistorted = imcube(img_undistorted, cubexy, corr) 
+        imshow(img_undistorted, "undistorted") 
+        imsave(img_undistorted, '../results/cube_dots_undistorted.png')
+        #plt.imshow(img_undistorted); plt.show() 
+    
+    if 0:
+        ### distorted images ### 
+        Rt = poseVector2TransformationMatrix(poses[0])
+        cornersXYZ = createGridCorners(args['grid_shape'], args['unit_size']) 
+        cornersxy = projectPoints(K, Rt, cornersXYZ)
+        cubeXYZ, corr = createCubeCorners(Oyx=(2,3), size=3, unit_size=args['unit_size']) 
+        cubexy = projectPoints(K, Rt, cubeXYZ) 
 
-def projectPoints(K, Rt, Pw):
-    Pw = np.concatenate([Pw, np.ones((1,len(Pw))).T], axis=1)
+        img = imread(filenames[0]) 
+        img = imdots(img, distort(cornersxy, K, D))
+        img = imcube(img, distort(cubexy, K, D),  corr) 
+
+        imshow(img, "distorted")  
+        imsave(img_undistorted, '../results/cube_dots_distorted.png')
+        plt.imshow(img); plt.show() 
     
-    p = K @ (Rt @ Pw.T)[:-1] 
-     
-    p = p / p[2]
+    if 0:
+        ### undistortion ### 
+        img = imread(filenames[0]) 
+        img0 = undistort(img, K, D, bilinear_interpolation=False)  
+        imshow(img0, "undistorted") 
+
+        img1 = undistort(img, K, D, bilinear_interpolation=True)  
+        imshow(img1, "undistorted") 
+
+        imcompare(img0, img1, 'without bilinear interpolation', 'with bilinear interpolation')   
     
-    return p[:2]
+    if 0:
+        ### movie ###
+        show_movie(poses, K, D, filenames)
+        create_movie(poses, K, D, filenames, shape=img.shape) 
 
 def distort(p, K, D):
-    pc = K[0:2,2].reshape(2,1) 
+    '''
+    Input:
+        - p(2, N): pixel coordinate of input points 
+        - K(3,3): the camera parameters 
+        - D(2)
+    
+    Output:
+        - pd(2, N): distorted pixel coordinates of points 
+    '''
+    pc = K[0:2,-1].reshape(2,1)                     # the center of distortion from K 
 
     r2 = (p[0] - pc[0])**2 + (p[1] - pc[1])**2
     r4 = r2**2
 
-    pd = (1 + D[0]*r2 + D[1]*r4) * (p - pc) + pc 
+    pd = (1 + D[0]*r2 + D[1]*r4) * (p - pc) + pc    
 
     return pd 
 
@@ -152,43 +120,174 @@ def undistort(img_d, K, D, bilinear_interpolation=False):
             else:
                 if 0 <= u1 and u1 < width and 0 <= v1 and v1 < height:
                     img[v, u] = img_d[v1, u1] 
+    return img
+
+
+def createGridCorners(shape=(6,9), unit_size=0.04):
+
+    Y = np.arange(shape[0]) * unit_size 
+    X = np.arange(shape[1]) * unit_size
+
+    YY, XX = np.meshgrid(Y, X) 
+    ZZ = YY*0 + XX*0  
+ 
+    XYZ = np.stack((XX, YY, ZZ), axis=-1).reshape(-1, 3).T
+
+    if 1:
+        print('XX = \n', XX) 
+        print('YY = \n', YY) 
+        print('ZZ = \n', ZZ)
+        print('np.stack((XX,YY,ZZ),axis=-1)=\n', np.stack((XX, YY, ZZ), axis=-1))
+        print('XYZ = \n', XYZ) 
+        
+    return XYZ 
+
+def createCubeCorners(Oyx=(0,0), size=1, unit_size=0.04):
+
+    Y = np.array([Oyx[0], Oyx[0]+size]) * unit_size # vertical 
+    X = np.array([Oyx[1], Oyx[1]+size]) * unit_size # horizontal 
+
+    YY, XX = np.meshgrid(Y, X) 
+    ZZbot = YY*0 + XX*0  
+    ZZtop = -0.5 * size * (np.ones_like(YY) * unit_size + np.ones_like(XX) * unit_size) 
+
+    XYZbot = np.stack((XX, YY, ZZbot), axis=-1).reshape(-1, 3).T 
+    XYZtop = np.stack((XX, YY, ZZtop), axis=-1).reshape(-1, 3).T 
+
+    XYZ = np.concatenate((XYZbot, XYZtop),axis=-1)
+
+    corr = np.array([[0, 0, 1, 2, 4, 4, 5, 6, 0, 1, 2, 3],
+                    [1, 2, 3, 3, 5, 6, 7, 7, 4, 5, 6, 7]],dtype=np.uint8)  
+
+    return XYZ, corr 
+
+def projectPoints(K, Rt, Pw):
+    '''project points onto screen 
+    Input:
+        - K(3,3): a camera matrix
+        - Rt(3,4): a transformatino matrix 
+        - Pw(3, N): N 3D world coodinates of points 
+    
+    Output:
+        - p(2, N): N pixel coordinates of points 
+    '''
+    
+    p = K @ Rt @ homogenous(Pw)
+     
+    p /= p[2]
+    
+    return p[:2]
+
+def poseVector2TransformationMatrix(pose):
+
+    R = rotVec2Mat(pose[:3])  
+    t = pose[3:].reshape(3,1) 
+    Rt = np.hstack((R, t))
+
+    if 0:
+        print(R) 
+        print(t)  
+        print(Rt) 
+    
+    return Rt
+
+def homogenous(X):
+    '''
+    Input:
+        - X(n, N): N n-dimensional coordinates 
+
+    Output:
+        - Xh(n+1, N): their hogenous coordinates 
+    '''
+    return np.vstack((X, np.ones(X.shape[1]))) 
+
+def rotVec2Mat(w): 
+    theta = np.linalg.norm(w)
+
+    k = w / theta
+
+    kx = np.array([
+        [0, -k[2], k[1]],
+        [k[2], 0, -k[0]],
+        [-k[1], k[0], 0]
+    ])
+
+    R = np.eye(3) + np.sin(theta) * kx + (1 - np.cos(theta)) * (kx@kx)
+    
+    return R 
+
+def imdots(img, p, r=5, color=(0,0,255), thickness=-1):
+    for x,y in zip(p[0], p[1]):
+        img = cv2.circle(img, (round(x), round(y)), r, color, thickness)
     return img 
 
-def load_data():
-    K = open("./data/K.txt").read()
-    D = open("./data/D.txt").read() 
-    poses = open("./data/poses.txt").read()
-    filenames = glob.glob('./data/images/*.jpg') 
+def imcube(img, p, corr, r=5, color=(0,0,255)):
+    for i, j in zip(corr[0], corr[1]):
+        x0 = round(p[0, i]); y0 = round(p[1, i])
+        x1 = round(p[0, j]); y1 = round(p[1, j])
+        img = cv2.line(img, (x0, y0), (x1, y1), color, r)
+    
+    return img 
 
-    K = txt2array(K).reshape(3,3) 
-    D = txt2array(D) 
-    P = txt2array(poses).reshape(len(poses.split('\n')[:-1]), 6)
-    filenames = sorted(filenames) 
+def implotXYZ(XX, YY, ZZ): # ZZ = f(XX, YY) 
+    fig = plt.figure()
+    ax = Axes3D(fig)
+    ax.plot_wireframe(XX, YY, ZZ)
+    plt.show()
 
-    return K, D, P, filenames
+def imcomparev(img1, img2, name1='img1', name2='img2', figsize=(10,10)): 
+    fig, axs = plt.subplots(nrows=2, sharex=True, figsize=figsize)  
+
+    axs[0].set_title(name1)
+    axs[0].imshow(img1)
+
+    axs[1].set_title(name2)
+    axs[1].imshow(img2)
+    plt.show()
 
 
-def show_undistorted_images(img, K, D):
-    img0 = undistort(img, K, D, bilinear_interpolation=False)  
-    imshow(img0, "undistorted") 
+def imcompare(img1, img2, name1='img1', name2='img2', figsize=(20,5)):
+    fig, axs = plt.subplots(1, 2, figsize=figsize)  
+    
+    axs[0].imshow(img1)
+    axs[0].set_title(name1)
 
-    img1 = undistort(img, K, D, bilinear_interpolation=True)  
-    imshow(img1, "undistorted") 
+    axs[1].imshow(img2)
+    axs[1].set_title(name2)
+ 
+    plt.show()
 
-    imshow2([img0, img1]) 
+    #plt.savefig(title) 
+
+def imshow(img, name="img"):
+    cv2.imshow(name, img)
+    while True:
+        if cv2.waitKey(1)==ord('q'): break 
+    cv2.destroyAllWindows()
+
+def imsave(img, name='img.png'):
+    cv2.imwrite(name, img) 
+
+def imread(filename):
+    return cv2.imread(filename)  
+
+def txt2array(filename, sep=' '):
+    txt = open(filename).read() 
+    return np.fromstring(txt, dtype=float, sep=sep)
+
 
 def show_movie(P, K, D, filenames):
     for i in range(len(filenames)):
         img = cv2.imread(filenames[i]) 
         Rt = poseVector2TransformationMatrix(P[i])
 
-        corners_w = create_corner_dots()
+        corners_w = createGridCorners() 
         corners_px = projectPoints(K, Rt, corners_w)
-        img = add_corners(img, distort(corners_px, K, D))
+        img = imdots(img, distort(corners_px, K, D))
 
-        cube_w = create_cube_dots(xy=(2,3), size=3)
+        cube_w = createCubeCorners(xy=(2,3), size=3)
         cube_px = projectPoints(K, Rt, cube_w) 
-        img = add_cube(img, distort(cube_px, K, D)) 
+        img = imcube(img, distort(cube_px, K, D)) 
 
         imshow(img, "distorted") 
 
@@ -201,13 +300,13 @@ def create_movie(P, K, D, filenames, shape=(480, 720, 3), fps=30, videoname='vid
         img = cv2.imread(filenames[i]) 
         Rt = poseVector2TransformationMatrix(P[i])
 
-        corners_w = create_corner_dots()
+        corners_w = createGridCorners()
         corners_px = projectPoints(K, Rt, corners_w)
-        img = add_corners(img, distort(corners_px, K, D))
+        img = imdots(img, distort(corners_px, K, D))
 
-        cube_w = create_cube_dots(xy=(2,3), size=3)
-        cube_px = projectPoints(K, Rt, cube_w) 
-        img = add_cube(img, distort(cube_px, K, D)) 
+        cube_w = createCubeCorners(xy=(2,3), size=3)
+        cube_px, corr = projectPoints(K, Rt, cube_w) 
+        img = imcube(img, distort(cube_px, K, D)) 
         
         frame_array[i] = img
 
@@ -215,41 +314,6 @@ def create_movie(P, K, D, filenames, shape=(480, 720, 3), fps=30, videoname='vid
     for i in range(len(frame_array)):
         out.write(frame_array[i])
     out.release()
-
-def main():
-    K, D, P, filenames = load_data()  
-
-    ### undistorted image ###
-
-    img_undistorted = cv2.imread('./data/images_undistorted/img_0001.jpg')  
-    Rt = poseVector2TransformationMatrix(P[0])
-
-    corners_w = create_corner_dots()
-    corners_px = projectPoints(K, Rt, corners_w)
-    img_undistorted = add_corners(img_undistorted, corners_px) 
-
-    cube_w = create_cube_dots(xy=(2,3), size=3)
-    cube_px = projectPoints(K, Rt, cube_w) 
-    img_undistorted = add_cube(img_undistorted, cube_px) 
-    
-    imshow(img_undistorted, "undistorted") 
-    plt.imshow(img_undistorted); plt.show() 
-
-    ### distorted images ### 
-
-    img = cv2.imread(filenames[0]) 
-    img = add_corners(img, distort(corners_px, K, D))
-    img = add_cube(img, distort(cube_px, K, D)) 
-
-    imshow(img, "distorted")  
-    plt.imshow(img); plt.show() 
-
-    ### undistortion ### 
-    show_undistorted_images(img, K, D) 
-    
-    ### movie ###
-    show_movie(P, K, D, filenames)
-    create_movie(P, K, D, filenames, shape=img.shape) 
 
 if __name__=="__main__":
     main() 
